87b1814858c0f30271c556eafca81a20fe102055
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-16 23:09:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ import leafmap.kepler as leafmap
 
 m = leafmap.Map(center=[40.4173, -82.9071], zoom=6)
 
-# in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
-# config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
-# m.add_csv(in_csv, layer_name="hex_data", config=config)
+in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
+config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
+m.add_csv(in_csv, layer_name="hex_data", config=config)
 
 m.to_streamlit()
 
 time.sleep(20)
 st.markdown("Test")
 
-in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
-config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
-m.add_csv(in_csv, layer_name="hex_data", config=config)
+# in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
+# config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
+# m.add_csv(in_csv, layer_name="hex_data", config=config)
 
 
 # "## Create a heat map"
