40d91917a40fc4dcd7707de5492fff04118abfef
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-16 21:10:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+import unfolded.map_sdk import UnfoldedMap
+
 st.markdown("### WEN-OKN: Dive into Data, Never Easier")
 
-map = create_map(renderer = "widget")
+map = UnfoldedMap()
