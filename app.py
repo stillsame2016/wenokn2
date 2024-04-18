dd8461643fb8444641182ea79871a68cbe386ac3
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-18 14:31:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
+
 import streamlit as st
-from streamlit_keplergl import keplergl_static
-from keplergl import KeplerGl
+import time
+import streamlit.components.v1 as components
 
-st.write("This is a kepler.gl map with specified configuration in streamlit")
+import streamlit as st
+import leafmap.kepler as leafmap
+from leafmap.common import get_center
 
-config = {
-    "version": "v1",
-    "config": {
-        "mapState": {
-            "bearing": 0,
-            "latitude": 52.52,
-            "longitude": 13.4,
-            "pitch": 0,
-            "zoom": 11,
-        }
-    },
-}
-map_1 = KeplerGl()
-map_1.config = config
 
-keplergl_static(map_1)
+st.set_page_config(layout="wide")
+st.markdown("### WEN-OKN: Dive into Data, Never Easier")
+
+m = leafmap.Map(center=[40.4173, -82.9071], zoom=6, height=400)
+
+in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
+config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
+m.add_csv(in_csv, layer_name="hex_data", config=config)
+
+m.to_streamlit()
+st.markdown(m.config)
+
+iframe_src = "https://open.spotify.com/embed/track/59BweHnnNQc5Y55WO30JuK?utm_source=generator"
+components.iframe(iframe_src)
