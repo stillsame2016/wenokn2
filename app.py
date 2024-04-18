2c5faffc4f68c523a105137ec9b864b22481adec
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-18 15:28:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
-
 import streamlit as st
-import time
-import streamlit.components.v1 as components
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
 
-import streamlit as st
-import leafmap.kepler as leafmap
-from leafmap.common import get_center
+react_app_url = "https://sparcal.sdsc.edu/build/"
 
-
-# st.set_page_config(layout="wide")
-st.markdown("### WEN-OKN: Test")
-
-m = leafmap.Map(center=[40.4173, -82.9071], zoom=6, height=300)
-
-in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
-config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
-m.add_csv(in_csv, layer_name="hex_data", config=config)
-
-m.to_streamlit()
-st.markdown(m.config)
-
-iframe_src = "https://open.spotify.com/embed/track/59BweHnnNQc5Y55WO30JuK?utm_source=generator"
-components.iframe(iframe_src)
+# Embed the React app in an iframe
+st.components.iframe(react_app_url, height=400)
