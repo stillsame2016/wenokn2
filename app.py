945e53cd784d93c468a9c87d472b3acfe29d91e8
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-18 15:30:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 react_app_url = "https://sparcal.sdsc.edu/build/"
 
 # Embed the React app in an iframe
-st.components.iframe(react_app_url, height=400)
+components.iframe(react_app_url, height=400)
