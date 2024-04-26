77e96480bb9deccc53f175b26863c2eb659d5ba1
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-26 16:55:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import streamlit as st
-import streamlit.components.v1 as components
+# import streamlit as st
+# import streamlit.components.v1 as components
 
-react_app_url = "https://sparcal.sdsc.edu/build/"
+# react_app_url = "https://sparcal.sdsc.edu/build/"
 
-# Embed the React app in an iframe
-components.iframe(react_app_url, height=400)
+# # Embed the React app in an iframe
+# components.iframe(react_app_url, height=400)
+
+import geemap.kepler as geemap
+
+m = geemap.Map(center=[40, -100], zoom=2, height=600, widescreen=False)
+m.to_streamlit(width=800, height=600)
+
