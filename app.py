50dc5d6ed5bd278b9f23d113951eb9459470b1ad
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-26 16:58:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,13 @@
 # components.iframe(react_app_url, height=400)
 
 import geemap.kepler as geemap
+import time
 
 m = geemap.Map(center=[40, -100], zoom=2, height=600, widescreen=False)
 m.to_streamlit(width=800, height=600)
+
+time.sleep(10000)
+
+polygons = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_states.json"
+m.add_geojson(polygons, layer_name="Countries")
 
