fc300a14b45ef4dfffd43e4740f9031fae1b71cf
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-26 17:00:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import streamlit as st
+import streamlit as st
 # import streamlit.components.v1 as components
 
 # react_app_url = "https://sparcal.sdsc.edu/build/"
@@ -10,10 +10,9 @@ import geemap.kepler as geemap
 import time
 
 m = geemap.Map(center=[40, -100], zoom=2, height=600, widescreen=False)
-m.to_streamlit(width=800, height=600)
-
-time.sleep(10000)
+m.to_streamlit(width=600, height=400)
 
 polygons = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_states.json"
 m.add_geojson(polygons, layer_name="Countries")
+st.markdown("Okay")
 
