f8235882c8ee42a212254e1b2e126ff3eaa16721
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-28 15:09:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 # import streamlit.components.v1 as components
 
 # react_app_url = "https://sparcal.sdsc.edu/build/"
@@ -12,6 +14,17 @@ import time
 m = geemap.Map(center=[40, -100], zoom=2, height=600, widescreen=False)
 m.to_streamlit(width=600, height=400)
 
+df = pd.DataFrame(
+    {
+        "City": ["San Francisco", "San Jose", "Palo Alto"],
+        "Latitude": [37.77, 37.33, 37.44],
+        "Longitude": [-122.43, -121.89, -122.14],
+    }
+)
+
+m.add_data(
+    data=df, name="cities"
+) 
 
 
 
