47a558d55e3400cfb2a895697351dd7424a3fc22
klin <klin@sdsc.edu>
2024-04-30 11:44:05 -0700

    update

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import json
 import streamlit as st
 import pandas as pd
 
+from sample_data import data
+
 from Kepler import my_component
+
 
 st.subheader("Kepler Bidirectional Connection Demo")
 
 datasets = []
+datasets.append({
+    "info": {"label": "Test Dataset", "id": "test-dataset"},
+    "data": data
+})
+
 map_config = my_component(json.dumps(datasets), key="map1")
 
 if map_config:
