ebe42c81c33f2da36851ebc1e210d472c226360d
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-02 14:20:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from Kepler import my_component
 from bart import data
+from san_diego import san_diego
 
 st.subheader("Kepler Bidirectional Communication Demo")
 
@@ -12,6 +13,8 @@ if "datasets" not in st.session_state:
     st.session_state.datasets = []
 
 map_config = my_component(json.dumps(st.session_state.datasets), height=400, key="map1")
+time.sleep(1)
+session_data_ids = []
 if map_config:
     map_config_json = json.loads(map_config)
     st.code(json.dumps(map_config_json, indent=4))
@@ -29,12 +32,20 @@ if map_config:
     for i in reversed(indices_to_remove):
         del st.session_state.datasets[i]
 
-    # st.markdown(f"final: {len(st.session_state.datasets)}")
+    session_data_ids = [dataset['info']['id'] for dataset in st.session_state.datasets]
 
-button_value = st.button('Add Dataset')
-if button_value and len(st.session_state.datasets) == 0:
+san_diego_button_clicked = st.button('Add San Diego Dataset', disabled=("san-diego" in session_data_ids))
+if san_diego_button_clicked:
     st.session_state.datasets.append({
-        "info": {"label": "Bart Stops Geo", "id": "bart-stops-geo"},
+        "info": {"label": "San Diego", "id": "san-diego"},
+        "data": san_diego
+    })
+    st.rerun()
+
+bart_button_clicked = st.button('Add Bart Dataset', disabled=("bart-stops" in session_data_ids))
+if bart_button_clicked:
+    st.session_state.datasets.append({
+        "info": {"label": "Bart Stops", "id": "bart-stops"},
         "data": data
     })
     st.rerun()
