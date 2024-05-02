eeba334b48d1dc681319dba471f060bb62de2be8
klin <klin@sdsc.edu>
2024-05-02 08:13:48 -0700

    Update __init__.py

--- Kepler/__init__.py
+++ Kepler/__init__.py
@@ -45,7 +45,7 @@ else:
 # `declare_component` and call it done. The wrapper allows us to customize
 # our component's API: we can pre-process its input args, post-process its
 # output value, and add a docstring for users.
-def my_component(name, key=None):
+def my_component(name, height=400, key=None):
     """Create a new instance of "my_component".
 
     Parameters
@@ -72,7 +72,7 @@ def my_component(name, key=None):
     #
     # "default" is a special argument that specifies the initial return
     # value of the component before the user has interacted with it.
-    component_value = _component_func(name=name, key=key, default=0)
+    component_value = _component_func(name=name, height=height, key=key, default=0)
 
     # We could modify the value returned from the component if we wanted.
     # There's no need to do this in our simple example - but it's an option.
