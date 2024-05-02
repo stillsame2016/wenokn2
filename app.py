ccceeecba9a70457342c11b22e4563c0fb3eb211
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-02 08:49:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,15 @@ import json
 import streamlit as st
 import pandas as pd
 
+from Kepler import my_component
 from sample_data import data
 
-from Kepler import my_component
+st.subheader("Kepler Bidirectional Communication Demo")
 
+if "datasets" not in st.session_state:
+    st.session_state.datasets = []
 
-st.subheader("Kepler Bidirectional Connection Demo")
-
-datasets = []
-datasets.append({
-    "info": {"label": "Test Dataset", "id": "test-dataset"},
-    "data": data
-})
-
-map_config = my_component(json.dumps(datasets), height=400, key="map1")
+map_config = my_component(json.dumps(st.session_state.datasets), height=400, key="map1")
 
 if map_config:
    st.code(json.dumps(json.loads(map_config), indent=4))
