41a93573aa4b79bf45e70109451220896af966cb
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-06 11:50:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,26 +18,6 @@ st.markdown("### WEN-OKN: Dive into Data, Never Easier")
 if "datasets" not in st.session_state:
     st.session_state.datasets = []
 
-# Setup the map
-options = {"keepExistingConfig": True}
-map_config = keplergl(st.session_state.datasets, options=options, config=None, height=400)
-time.sleep(0.5)
-
-# Sync datasets and the map
-session_data_ids = []
-if map_config:
-    map_config_json = json.loads(map_config)
-
-    # check if any datasets were deleted
-    map_data_ids = [layer["config"]["dataId"] for layer in map_config_json["visState"]["layers"]]
-    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
-    indices_to_remove = [i for i, dataset in enumerate(st.session_state.datasets) if not dataset.id in map_data_ids]
-    for i in reversed(indices_to_remove):
-        del st.session_state.datasets[i]
-
-    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
-    # st.markdown(session_data_ids)
-
 # Setup LLM model
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -88,25 +68,40 @@ def df_to_gdf(df):
   return gdf
 
 
+col1, col2 = st.columns([6, 4])
 
-col1, col2, col3 = st.columns([1, 1, 1])
-with col1:
-    san_diego_button_clicked = st.button('Add Bart Stops Geo', disabled=("bart-stops-geo" in session_data_ids))
-    if san_diego_button_clicked:
-        st.session_state.datasets.append(bart_stops_geo_gdf)
-        st.rerun()
+info_container = st.container(height=350)
+with info_container:
+  for idx, sparql in enumerate(st.session_state.sparqls): 
+    st.markdown(f"**Request:**  {st.session_state.requests[idx]}")
+    st.code(sparql)
+
+with col1:  
+  # Setup the map
+options = {"keepExistingConfig": True}
+map_config = keplergl(st.session_state.datasets, options=options, config=None, height=400)
+time.sleep(0.5)
+
+# Sync datasets and the map
+session_data_ids = []
+if map_config:
+    map_config_json = json.loads(map_config)
+
+    # check if any datasets were deleted
+    map_data_ids = [layer["config"]["dataId"] for layer in map_config_json["visState"]["layers"]]
+    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
+    indices_to_remove = [i for i, dataset in enumerate(st.session_state.datasets) if not dataset.id in map_data_ids]
+    for i in reversed(indices_to_remove):
+        del st.session_state.datasets[i]
+
+    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
+    # st.markdown(session_data_ids)
 
 with col2:
-    bart_button_clicked = st.button('Add SF Zip Geo', disabled=("sf-zip-geo" in session_data_ids))
-    if bart_button_clicked:
-        st.session_state.datasets.append(sf_zip_geo_gdf)
-        st.rerun()
+    
+  # Create a container for the chat messages
+  chat_container = st.container(height=350)
 
-with col3:
-    h3_button_clicked = st.button('Add H3 Hexagons V2', disabled=("h3-hex-id" in session_data_ids))
-    if h3_button_clicked:
-        st.session_state.datasets.append(h3_hex_id_df)
-        st.rerun()
 
 if map_config:
     st.code(json.dumps(map_config_json, indent=4))
