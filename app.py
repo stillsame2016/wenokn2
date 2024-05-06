5f530698cb9be0f931d0e7bb3f60ba97776b8aed
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-06 15:04:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import os
-import sys
-
-# Add the project root directory to the sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, project_root)
-
 import time
 import json
 import re
