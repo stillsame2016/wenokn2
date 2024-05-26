badb53eddf3735146c0c81b0e80e171b8b28540c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-26 14:50:38 -0700

    Update request_plan.py

--- request_plan.py
+++ request_plan.py
@@ -6,24 +6,26 @@ from langchain_core.output_parsers import JsonOutputParser
 # Implement the Request Planer
 def get_request_plan(llm, question):
     prompt = PromptTemplate(
-        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are the export of WEN-OKN knowledge 
-            system which answer one question or return data for one entity type at a time. For example, we can return 
-            dams or return earthquakes. You need to extract a list of atomic requests from the user's question and 
-            also make each atomic request can be executed independent to other atomic requests. 
+        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are the export of 
+            WEN-OKN knowledge system which answer one question or return data for one entity type at 
+            a time. For example, we can return dams or return earthquakes. You need to extract a list 
+            of atomic requests from the user's question and also make each atomic request can be 
+            executed independent to other atomic requests. 
 
             [Example 1]
             First find Scioto River and all dams on it. 
             
-            This question requests to find a river entity and some dam entities. So it can be divided into the following two 
-            atomic requests:
+            This question requests to find a river entity and some dam entities satisfying some conditions. 
+            So it can be divided into the following two atomic requests for two types of entities:
                Find Scioto River
                Find all dams on Scioto River
             
             [Example 2]
             First find Scioto River, then find all dams on this river. Also find all counties these dams locate. 
             
-            This question requests to find a river entity and some dam entities and some county entities. So it can be divided 
-            into the following three atomic requests:
+            This question requests to find a river entity and some dam entities and some county entities 
+            satisfying some conditions. So it can be divided into the following three atomic requests for three
+            entity types:
                Find Scioto River
                Find all dams on Scioto River
                Find all counties where dams on Scioto River are located
@@ -31,7 +33,16 @@ def get_request_plan(llm, question):
             [Example 3]
             Find all counties Scioto River flows through
             
-            This question requests to find some county entities. So it is already an atomic request.
+            This question requests to find some county entities satisfying some conditions. So it is already an 
+            atomic request.
+
+            [Example 4]
+            Find all dams on Scioto River
+
+            This is already an atomic request because it requests to find dam entities satisfying some conditions.
+            
+            This question requests to find some county entities satisfying some conditions. So it is already an 
+            atomic request.
 
             [Question]
             The following is the question from the user:
