71094427b79f34c6f1a9b972a941ed3be594c26c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-29 21:36:28 -0700

    Create data_commons.py

---
+++ data_commons.py
@@ -0,0 +1,9 @@
+
+import datacommons_pandas as dc
+            
+def get_variables_for_fips(fips_list, variable_name_list):
+    _df = dc.build_multivariate_dataframe(fips_list, variable_list)
+    _df['name'] = _df.index.map(dc.get_property_values(_df.index, 'name'))
+    _df['name'] = _df['name'].str[0]
+    _df['fips'] = _df.index.to_series().apply(lambda x: x.replace("geoId/", ""))
+    return _df
