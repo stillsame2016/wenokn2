44e81d4db97d158a438d0a7b4a022237efce6f77
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-30 14:23:09 -0700

    Update data_commons.py

--- data_commons.py
+++ data_commons.py
@@ -4,15 +4,15 @@ import datacommons_pandas as dc
 def get_variables_for_fips(fips_list, variable_name_list):
     _df = dc.build_multivariate_dataframe(fips_list, variable_name_list)
     # _df['name'] = _df.index.map(dc.get_property_values(_df.index, 'name'))
-    _df.insert(0, 'name', _df.index.map(dc.get_property_values(_df.index, 'name')))        
-    _df['name'] = _df['name'].str[0]
+    _df.insert(0, 'Name', _df.index.map(dc.get_property_values(_df.index, 'name')))        
+    _df['Name'] = _df['Name'].str[0]
     # _df['fips'] = _df.index.to_series().apply(lambda x: x.replace("geoId/", ""))
     return _df
 
 def get_time_series_dataframe_for_fips(fips_list, variable_name):
     _df = dc.build_time_series_dataframe(fips_list, variable_name)
     # _df['name'] = _df.index.map(dc.get_property_values(_df.index, 'name'))
-    _df.insert(0, 'name', _df.index.map(dc.get_property_values(_df.index, 'name')))
-    _df['name'] = _df['name'].str[0]
+    _df.insert(0, 'Name', _df.index.map(dc.get_property_values(_df.index, 'name')))
+    _df['Name'] = _df['Name'].str[0]
     # _df['fips'] = _df.index.to_series().apply(lambda x: x.replace("geoId/", ""))
     return _df
