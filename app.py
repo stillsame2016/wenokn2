4df4225e4f5a3d652207cb8917eccd83e77e1f16
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-31 15:17:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ if st.session_state.wen_datasets:
             st.write(f"<div class='tableTitle'>Table {index+1}: {dataset.title}</div>", unsafe_allow_html=True)
             ''
             columns = dataset.columns.to_list().remove('Name')
-            st.code(columns)
             pivot_table = table.melt(
                 ['Name'],
                 columns,
@@ -109,7 +108,7 @@ if st.session_state.wen_datasets:
                 #                         max_value=max_value,
                 #                         value=[min_value, max_value])
     
-                selected_counties = ['Pike County', 'Ross County', 'Adams County', 'Allen County', 'Ashland County', 'Ashtabula County', 'Athens County', 'Auglaize County', 'Belmont County', 'Brown County', 'Butler County', 'Carroll County' ]
+                selected_counties = dataset['Name']
                 # Filter the data
                 filtered_gdp_df = pivot_table[
                     (pivot_table['Name'].isin(selected_counties))
