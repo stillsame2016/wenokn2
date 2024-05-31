cad48811e827d5173b5d0dab233525201dc9cb1c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-30 22:33:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def add_map():
 if st.session_state.wen_datasets:
     for index, dataset in enumerate(st.session_state.wen_datasets):
         table = st.session_state.wen_datasets[index]
-
+        st.divider()
         with st.container():  
             st.markdown("""
                             <style>
