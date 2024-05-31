2df8a677d538a6c262ec1f8449ebfc66e02ac984
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-30 18:17:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if st.session_state.wen_datasets:
             st.write(f"<div class='tableTitle'>Table {index+1}: {dataset.id}</div>", unsafe_allow_html=True)
 
             min_value, max_value = 1970, 2022
-            from_year, to_year = st.slider("Select a time ranger",
+            from_year, to_year = st.slider("Select a time range",
                                     min_value=min_value,
                                     max_value=max_value,
                                     value=[min_value, max_value])
