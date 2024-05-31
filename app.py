2b23b93599e3a23c6b20c5bb9651f552c7a2dc09
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-30 18:04:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if st.session_state.wen_datasets:
                             }
 
                             .stSlider [data-baseweb=slider]{
-                                width: 95%;
+                                width: 92%;
                                 margin: 0px 50px 0px 50px;
                             }
 
