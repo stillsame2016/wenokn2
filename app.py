a74c0e3d449bdb8c76b5c08395b62b1ee8b0ec2b
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-31 15:13:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,8 @@ if st.session_state.wen_datasets:
                         """, unsafe_allow_html=True)
             st.write(f"<div class='tableTitle'>Table {index+1}: {dataset.title}</div>", unsafe_allow_html=True)
             ''
-            columns = dataset.columns.remove('Name')
+            columns = dataset.columns
+            columns.remove('Name')
             pivot_table = table.melt(
                 ['Name'],
                 columns,
