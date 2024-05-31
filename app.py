1133119f697d82ffa636a9165e30f50d7e9b2ad6
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-30 22:36:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ def add_map():
 if st.session_state.wen_datasets:
     for index, dataset in enumerate(st.session_state.wen_datasets):
         table = st.session_state.wen_datasets[index]
-        st.divider()
-        with st.container():  
+        with st.container(): 
+            st.write("<hr/>", unsafe_allow_html=True)
             st.markdown("""
                             <style>
                             .tableTitle {
