9006eba3244bb2808d7683db1f9eb156bceccd13
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-31 22:37:58 -0700

    Update data_commons.py

--- data_commons.py
+++ data_commons.py
@@ -13,7 +13,7 @@ def get_variables_for_fips(fips_list, variable_name_list):
 
 def get_time_series_dataframe_for_fips(fips_list, variable_name):
     _df = dc.build_time_series_dataframe(fips_list, variable_name)
-    # _df = _df.fillna(0)
+    _df = _df.fillna(0)
     # _df['name'] = _df.index.map(dc.get_property_values(_df.index, 'name'))
     # _df['fips'] = _df.index.to_series().apply(lambda x: x.replace("geoId/", ""))
     _df.insert(0, 'Name', _df.index.map(dc.get_property_values(_df.index, 'name')))
