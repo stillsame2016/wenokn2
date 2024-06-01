d3e3c5855b1a5a3dae6c2174a19db2dedd4bbba8
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-31 22:33:49 -0700

    Update data_commons.py

--- data_commons.py
+++ data_commons.py
@@ -36,7 +36,7 @@ def get_fips_from_county_name(county_name):
         # Execute the simple query
         fips_dict = dc.query(simple_query)
         fips = [ item['?geoId'] for item in fips_dict ]
-        return fips
+        return fips[0]
     except Exception as ex:
         return None
      
