098a7911a9694a64c80a249f88121b753bed7056
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-31 22:21:38 -0700

    Update data_commons.py

--- data_commons.py
+++ data_commons.py
@@ -10,6 +10,7 @@ def get_variables_for_fips(fips_list, variable_name_list):
     # _df['fips'] = _df.index.to_series().apply(lambda x: x.replace("geoId/", ""))
     return _df
 
+
 def get_time_series_dataframe_for_fips(fips_list, variable_name):
     _df = dc.build_time_series_dataframe(fips_list, variable_name)
     # _df = _df.fillna(0)
@@ -19,3 +20,23 @@ def get_time_series_dataframe_for_fips(fips_list, variable_name):
     _df['Name'] = _df['Name'].str[0]
     _df.variable_name = variable_name
     return _df
+
+
+def get_fips_from_county_name(county_name):
+    simple_query = f"""
+                    SELECT ?geoId
+                    WHERE {
+                      ?county typeOf City .
+                      ?county name '{county_name}' .
+                      ?county dcid ?geoId .
+                    }
+                    LIMIT 1
+                 """
+    try:
+        # Execute the simple query
+        fips_dict = dc.query(simple_query)
+        fips = [ item['?geoId'] for item in fips_dict ]
+        return fips
+    except Exception as ex:
+        return None
+     
