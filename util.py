0b0d61178b18c290a229eddcef03fb76dd7e768c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-02 12:16:34 -0700

    Update util.py

--- util.py
+++ util.py
@@ -361,7 +361,7 @@ def process_table_request(llm, user_input, index):
     df_code_chain = prompt | llm | JsonOutputParser()
     return df_code_chain.invoke({"index": index,
                                  "columns": st.session_state.wen_datasets[index].columns.to_list(), 
-                                 "question": question})
+                                 "question": user_input})
 
 
 
