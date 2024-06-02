e8c555e4ed2e88b62834332c32bc6d0f44be016e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-02 10:47:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if st.session_state.wen_datasets:
                     }
                 </style>
             """, unsafe_allow_html=True)
-            but_col1, but_col2 = st.columns(2)
+            but_col1, but_col2, but_pad = st.columns([1,1,10])
             with but_col1:
                 if st.button('Delete', key=f'delete-table-{index}'):
                     del st.session_state.wen_datasets[index]
