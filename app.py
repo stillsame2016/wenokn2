19e72f3aba789318346f1f531cd1598eace628d1
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-01 21:26:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if st.session_state.wen_datasets:
                 height=450
             )
             
-            col3, pad, col4 = st.columns([3, 1, 3])
+            col3, pad, col4 = st.columns([6, 1, 6])
             with col3:
                 st.dataframe(pivot_table, hide_index=True, use_container_width=True)
                 # st.dataframe(table, width=1100, hide_index=True)
