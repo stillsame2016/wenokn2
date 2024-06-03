475f7cd3cfb6d9ac017c43ae95a0c47a41b54cc6
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-02 21:06:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -188,8 +188,6 @@ if st.session_state.wen_datasets:
                                         """
                             st.chat_message("assistant").markdown(answer)
                             st.session_state.table_chat_histories[index].append({"role": "assistant", "content": answer})
-
-                            time.sleep(2)
                             st.rerun()
                         else:
                             st.chat_message("assistant").markdown(response['answer'])
