57275ee47711e6bd2fbb4db2b4acf90c04459626
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-05 10:51:58 -0700

    Update util.py

--- util.py
+++ util.py
@@ -216,10 +216,9 @@ def process_data_commons_request(llm, user_input, spatial_datasets):
     variables = ""
     if spatial_datasets:
         for index, dataset in enumerate(spatial_datasets):
-            pass
-    #         variables += f"""
-    #                          st.session_state.datasets[{index}] : { st.session_state.datasets[index]..label}
-    #                       """
+            variables += f"""
+                             st.session_state.datasets[{index}] : { st.session_state.datasets[index].label}
+                          """
         
     return df_code_chain.invoke({"question": user_input, "variables": variables})
     
