69f020e924f2b5601726196b48da363900c3451f
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-09 20:36:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if st.session_state.wen_datasets:
                             st.code(result.columns)
                             
                             # Select the desired columns, including geometry and the original columns from df
-                            result = result[['Name_y', 'geometry'] + df.columns[1:].to_list()]
+                            result = result[['Name_y', 'geometry'] + df.columns[1:-1].to_list()]
                             
                             # Optionally, rename 'Name_y' back to 'Name'
                             result = result.rename(columns={'Name_y': 'Name'})
