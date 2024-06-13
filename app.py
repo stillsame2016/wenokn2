14d9c18bd7dfa41077f58f162ebed709d9f496b4
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-13 10:37:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ col1, col2 = st.columns([3, 2])
 
 @st.experimental_fragment
 def add_map():
-    # st.markdown(f"st.session_state.datasets: {len(st.session_state.datasets)}")
+    st.markdown(f"st.session_state.datasets: {len(st.session_state.datasets)}")
     options = {"keepExistingConfig": True}
     _map_config = keplergl(st.session_state.datasets, options=options, config=None, height=410)
     time.sleep(0.5)
