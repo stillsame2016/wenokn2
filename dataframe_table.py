fb49d09052a7f749e1e263c2f04b4dc9e39db1a8
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-14 11:17:29 -0700

    Update dataframe_table.py

--- dataframe_table.py
+++ dataframe_table.py
@@ -31,9 +31,7 @@ def render_interface_for_table(llm, llm2, index, pivot_table):
                 else:
                     st.session_state.chart_types[index] = 'bar_chart'
 
-
-
-        st.markdown("&nbsp;")
+        st.write(f"<div styple='height: 10px'></div>", unsafe_allow_html=True)
         if st.session_state.chart_types[index] == 'bar_chart':
             st.bar_chart(
                 buffered_table, # filtered_pivot_table,
