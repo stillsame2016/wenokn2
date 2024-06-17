2a0a47a629ca38fddefffec47d3f696c944c670b
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-16 18:07:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,9 @@ if len(st.session_state.sparqls) > 0:
     info_container = st.container(height=350)
     with info_container:
         for idx, sparql in enumerate(st.session_state.sparqls):
-            if st.session_state.requests[idx] != "":
+            if st.session_state.requests[idx] == "":
+                pass
+            else:
                 st.markdown(f"**Request:**  {st.session_state.requests[idx]}")
                 st.code(sparql)
 
