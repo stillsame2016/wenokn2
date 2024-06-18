6489cf7bdbb73c416ee26cd6b36101df6c975ae1
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-18 15:14:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from refine_request import get_refined_question
 from request_router import get_question_route
 from request_plan import get_request_plan
 from dataframe_table import render_interface_for_table
-from data_commons import get_time_series_dataframe_for_dcid, get_dcid_from_county_name,  get_dcid_from_state_name
+from data_commons import get_time_series_dataframe_for_dcid, get_dcid_from_county_name,  get_dcid_from_state_name, get_dcid_from_country_name
 
 
 # Setup LLM
