2f95d9be383046c1c1f9fd74b3eb3fcc16506994
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-30 18:30:58 -0700

    Update util.py

--- util.py
+++ util.py
@@ -546,7 +546,7 @@ def create_new_geodataframe(gdfs, df):
     return new_gdf
 
 def load_coal_mines(where):
-    url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/CoalMines_US_EIA/FeatureServer/247"
+    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/CoalMines_US_EIA/FeatureServer/247"
     url_string = self_url + "/query?where={}&returnGeometry=true&outFields={}&f=geojson".format(where, '*')
     resp = requests.get(url_string, verify=False)
     data = resp.json()
