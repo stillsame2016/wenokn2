b9aa7915384775565f6a44dbac2845d65f28d853
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-06-30 17:31:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import datacommons_pandas as dc
 from keplergl import keplergl
 from langchain_groq import ChatGroq
 
-from util import process_data_request, process_regulation_request, process_off_topic_request, process_data_commons_request
+from util import process_data_request, process_regulation_request, process_off_topic_request, process_data_commons_request, process_energy_atlas_request
 from refine_request import get_refined_question
 from request_router import get_question_route
 from request_plan import get_request_plan
