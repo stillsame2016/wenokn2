a40327c6c3af21d8398edda45fbec552309cee77
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-02 10:34:26 -0700

    Update util.py

--- util.py
+++ util.py
@@ -625,7 +625,7 @@ def load_features(self_url, where, wkid):
     if data:
         return gpd.GeoDataFrame.from_features(data['features'], crs=f'EPSG:{wkid}')
     else:
-        return gpd.GeoDataFrame(columns=['geometry'], crs=f'EPSG:{wkid}')
+        return gpd.GeoDataFrame(columns=['geometry'])
 
 def load_coal_mines(where):
     self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/CoalMines_US_EIA/FeatureServer/247"
