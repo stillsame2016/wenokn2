ac4fb4558404254c34f1f5aad4644f606b27bcea
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-02 14:27:56 -0700

    Update util.py

--- util.py
+++ util.py
@@ -634,37 +634,4 @@ def create_new_geodataframe(gdfs, df):
     return new_gdf
 
 
-def load_features(self_url, where, wkid):
-    url_string = self_url + "/query?where={}&returnGeometry=true&outFields={}&f=geojson".format(where, '*')
-    resp = requests.get(url_string, verify=False)
-    data = resp.json()
-    if data['features']:
-        return gpd.GeoDataFrame.from_features(data['features'], crs=f'EPSG:{wkid}')
-    else:
-        return gpd.GeoDataFrame(columns=['geometry'], crs=f'EPSG:{wkid}')
-
-def load_coal_mines(where):
-    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/CoalMines_US_EIA/FeatureServer/247"
-    wkid = "3857"
-    return load_features(self_url, where, wkid)
-
-def load_coal_power_plants(where):
-    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/Coal_Power_Plants/FeatureServer/0"
-    wkid = "3857"
-    return load_features(self_url, where, wkid)
-
-def load_wind_power_plants(where):
-    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/Wind_Power_Plants/FeatureServer/0"
-    wkid = "3857"
-    return load_features(self_url, where, wkid)
-
-def load_renewable_diesel_fuel_and_other_biofuel_plants(where):
-    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/Renewable_Diesel_and_Other_Biofuels/FeatureServer/245"
-    wkid = "3857"
-    return load_features(self_url, where, wkid)
-
-def load_battery_storage_plant(where):
-    self_url = "https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/Battery_Storage_Plants/FeatureServer/0"
-    wkid = "3857"
-    return load_features(self_url, where, wkid)
     
