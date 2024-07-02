fbf9ae4e5f03745dce07dd00a02b7b864c405518
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-01 17:54:59 -0700

    Update request_router.py

--- request_router.py
+++ request_router.py
@@ -51,6 +51,7 @@ def get_question_route(llm, question):
         Use US Energy Atlas for requests related to the following data:
             Coal Mine
             Coal Power Plant
+            Wind Power Plant
             
         Use Other for questions related to common knowledge. 
         
