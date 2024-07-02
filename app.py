a76ddf7cbfc1e737df917e9cbbe83923b5c621c6
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-02 11:43:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from request_router import get_question_route
 from request_plan import get_request_plan
 from dataframe_table import render_interface_for_table
 from data_commons import get_time_series_dataframe_for_dcid, get_dcid_from_county_name,  get_dcid_from_state_name, get_dcid_from_country_name
-from util import load_coal_mines, load_coal_power_plants, load_wind_power_plants, load_renewable_diesel_fuel_and_other_biofuel_plants
+from util import load_coal_mines, load_coal_power_plants, load_wind_power_plants, load_renewable_diesel_fuel_and_other_biofuel_plants, load_battery_storage_plant
 
 
 # Setup LLM
