280e6ccd7b7c86cfc9d40030e73a050ab586f25f
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-01 21:02:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -213,12 +213,12 @@ with col2:
                                         loaded on the map.
                                         """
                         except Exception as e:
-                            # message = f"""
-                            #            {code} 
-                            #            {str(e)}
-                            #            """  
-                            message = f"""We are not able to process your request. Please refine your 
-                                              request and try it again. \n\nError: {str(e)}"""
+                            message = f"""
+                                       {code} 
+                                       {str(e)}
+                                       """  
+                            # message = f"""We are not able to process your request. Please refine your 
+                            #                   request and try it again. \n\nError: {str(e)}"""
                     st.code(message)
                     st.session_state.chat.append({"role": "assistant", "content": message})
             else:
