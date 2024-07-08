42d5480aca51f83f1a85bb20b775568aaaee9d82
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-07 21:29:51 -0700

    Update util.py

--- util.py
+++ util.py
@@ -244,7 +244,7 @@ def process_data_commons_request(llm, user_input, spatial_datasets):
                                  { st.session_state.datasets[index].head(5).drop(columns='geometry').to_csv(index=False) }
                                  
                           """
-    st.code(variables)
+    # st.code(variables)
     return df_code_chain.invoke({"question": user_input, "variables": variables, "dc_variables": dc_variables})
     
 
