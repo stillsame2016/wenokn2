6a4d63dd50d53937ef60cd6dabfb63167a1fa242
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-10 15:26:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ with col2:
         user_input = init_query
 
     options = ['cat', 'dog', 'mouse', 'bat', 'duck']
-    option_selected = st.selectbox("Sample Queries", label_visibility=True, options)
+    option_selected = st.selectbox("Sample Queries", options, label_visibility=True)
     
     if user_input:
         with chat_container:
