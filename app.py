acaddf9acfe99b65f61e35a7b19b76f8c5bbdb9f
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-13 22:39:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,8 @@ if "sparqls" not in st.session_state:
 
 if "sample_query" not in st.session_state:
     st.session_state.sample_query = []
+if "selection_index" not in st.session_state:
+    st.session_state.selection_index = None
 
 # @st.experimental_fragment
 @st.fragment(run_every=60*5)
@@ -183,8 +185,9 @@ with col2:
                                  placeholder="Sample Queries",
                                  key='selection_index')
     if selected_item:
+        st.markdown(st.session_state.selected_item)
         st.session_state.sample_query = [ selected_item ]
-        st.session_state.selection_index = None
+        
         
     if user_input:
         with chat_container:
