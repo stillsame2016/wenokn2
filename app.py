773c26d88e20f8b7731751ad95074146632dfd2a
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-13 22:10:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ if st.session_state.sample_query:
                 const event = new Event('input', {{ bubbles: true }});
                 chatInput.dispatchEvent(event);
             }}
-            setTimeout(autoResizeTextarea, 2000)
+            setTimeout(autoResizeTextarea, 1000)
             </script>
             """
     html(js_code)
