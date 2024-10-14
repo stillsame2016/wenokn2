f879d3ccae3b4e4e7341257e789d21048c3e029b
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-14 10:28:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -349,6 +349,7 @@ if option != st.session_state.last_selected_option:
 
 # Create a button to clear the selectbox
 if st.button("Clear Selection", on_click=clear_selection):
+    time.sleep(3)
     st.rerun()
 
 # Display the current selection
