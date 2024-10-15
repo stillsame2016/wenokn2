d4e520bcd2e50793b88f1a5027cf2fff1a654b22
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-15 12:01:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ if st.session_state.sample_query:
                             clearButton.dispatchEvent(mouseDown);
                             clearButton.dispatchEvent(mouseUp);
                             clearButton.dispatchEvent(click);
-                        }}, 1500);
+                        }}, 1200);
                         obs.disconnect();
                     }}
                 }});
