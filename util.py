7dbedd3db6cd42493b7dfa5fc8ea169c08d57a68
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-15 10:45:08 -0700

    Update util.py

--- util.py
+++ util.py
@@ -153,9 +153,22 @@ def process_data_commons_request(llm, user_input, spatial_datasets):
             import datacommons_pandas as dc
             
             def get_time_series_dataframe_for_dcid(dcid_list, variable_name):
-                _df = dc.build_time_series_dataframe(dcid_list, variable_name)
+                _df = dc.build_time_series_dataframe(dcid_list, variable_name)    
                 _df.insert(0, 'Name', _df.index.map(dc.get_property_values(_df.index, 'name')))
-                _df['Name'] = _df['Name'].str[0]
+                _df['Name'] = _df['Name'].str[0]    
+                
+                # columns = _df.columns.to_list().remove('Name')
+                columns = _df.columns.to_list()
+                columns.remove('Name')
+                _df = _df.melt(
+                    ['Name'],
+                    columns,
+                    'Date',
+                     variable_name,
+                )
+                _df = _df.dropna()     
+                _df = _df.drop_duplicates(keep='first')
+                _df.variable_name = variable_name
                 return _df
                 
             [Example 1] 
