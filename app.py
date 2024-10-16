fa5568ca6bf96a639665a867eab19d4f56a0702a
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-16 00:25:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -296,12 +296,12 @@ with col2:
                                             this is a mistake.
                                             """
                         except Exception as e:
-                            message = f"""
-                                       {code} 
-                                       {str(e)}
-                                       """  
-                            # message = f"""We are not able to process your request. Please refine your 
-                            #                   request and try it again. \n\nError: {str(e)}"""
+                            # message = f"""
+                            #            {code} 
+                            #            {str(e)}
+                            #            """  
+                            message = f"""We are not able to process your request. Please refine your 
+                                              request and try it again. \n\nError: {str(e)}"""
                     st.markdown(message)
                     st.session_state.chat.append({"role": "assistant", "content": message})
             elif route['request_type'] == "WEN-KEN database use Energy Atlas":
