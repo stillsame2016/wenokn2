b5853ef69c0d7c92a19c4708ba5e1f89a3700b43
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-16 14:24:31 -0700

    Update wenokn_use_energy_atlas.py

--- wenokn_use_energy_atlas.py
+++ wenokn_use_energy_atlas.py
@@ -87,7 +87,7 @@ def process_wenokn_use_energy_atlas(llm, user_input):
         which are described using entities in Energy Atlas. Your task is to provide Python code which 
         converts this request into a request in natural language without using any conditions related 
         to energy Atlas as a Python variable converted_request. Please return the Python code only 
-        without any explanation. Don't include any print statement. Don't add ``` around the code.
+        without any explanation. Don't include any print statement. Don't add ``` or ```python around the code.
 
         [ Example ]
         Find all counties downstream of the coal mine with the name "Century Mine" along Ohio River.
