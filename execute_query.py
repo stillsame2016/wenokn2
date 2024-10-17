71367185b5daa990790fba4c8705f4e2b0195709
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-17 14:43:13 -0700

    Update execute_query.py

--- execute_query.py
+++ execute_query.py
@@ -34,7 +34,7 @@ def execute_query(user_input, chat_container, llm):
                                 code = code[start_index:end_index].strip()
                             st.code(code)
                             st.code(f"check: {len(st.session_state.datasets)}")
-                            exec(code)
+                            exec(code, globals(), locals())
                             if gdf.shape[0] > 0:
                                 if hasattr(gdf, 'answer'):
                                     message = gdf.answer
