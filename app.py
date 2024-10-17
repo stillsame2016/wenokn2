98cb7ff3d3e306cd708ef3906d2d2868bd35adb8
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-17 12:16:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -321,43 +321,44 @@ with col2:
                                     count_start = len(st.session_state.datasets)
                                     for query in query_plan:
                                         with chat_container:
-                                            st.markdown(f"Executing the query plan and process the query: *{query}*")
-                                            if query["data_source"] == "WEN-OKN Database":
-                                                process_data_request(query["request"], chat_container)
-                                            elif query["data_source"] == "Energy Atlas":
-                                                code = process_energy_atlas_request(llm, query["request"], st.session_state.datasets)
-                                                if code.startswith("```python"):
-                                                    start_index = code.find("```python") + len("```python")
-                                                    end_index = code.find("```", start_index)
-                                                    code = code[start_index:end_index].strip()
-                                                elif code.startswith("```"):
-                                                    start_index = code.find("```") + len("```")
-                                                    end_index = code.find("```", start_index)
-                                                    code = code[start_index:end_index].strip()
-                                                st.code(code)
-                                                exec(code)
-                                                if gdf.shape[0] > 0:
-                                                    if hasattr(gdf, 'answer'):
-                                                        message = gdf.answer
+                                            with st.chat_message("assistant"):
+                                                st.markdown(f"Executing the query plan and process the query: *{query}*")
+                                                if query["data_source"] == "WEN-OKN Database":
+                                                    process_data_request(query["request"], chat_container)
+                                                elif query["data_source"] == "Energy Atlas":
+                                                    code = process_energy_atlas_request(llm, query["request"], st.session_state.datasets)
+                                                    if code.startswith("```python"):
+                                                        start_index = code.find("```python") + len("```python")
+                                                        end_index = code.find("```", start_index)
+                                                        code = code[start_index:end_index].strip()
+                                                    elif code.startswith("```"):
+                                                        start_index = code.find("```") + len("```")
+                                                        end_index = code.find("```", start_index)
+                                                        code = code[start_index:end_index].strip()
+                                                    st.code(code)
+                                                    exec(code)
+                                                    if gdf.shape[0] > 0:
+                                                        if hasattr(gdf, 'answer'):
+                                                            message = gdf.answer
+                                                        else:
+                                                            gdf.label = gdf.title
+                                                            gdf.id = str(uuid.uuid4())[:8]
+                                                            gdf.time = time.time()
+                                                            st.session_state.requests.append(query["request"])
+                                                            st.session_state.sparqls.append("")
+                                                            st.session_state.datasets.append(gdf)
+                                                            # st.session_state.rerun = True
+                                                            message = f"""
+                                                                        Your request has been processed. {gdf.shape[0]} 
+                                                                        { "items are" if gdf.shape[0] > 1 else "item is"}
+                                                                        loaded on the map.
+                                                                        """
                                                     else:
-                                                        gdf.label = gdf.title
-                                                        gdf.id = str(uuid.uuid4())[:8]
-                                                        gdf.time = time.time()
-                                                        st.session_state.requests.append(query["request"])
-                                                        st.session_state.sparqls.append("")
-                                                        st.session_state.datasets.append(gdf)
-                                                        # st.session_state.rerun = True
                                                         message = f"""
-                                                                    Your request has been processed. {gdf.shape[0]} 
-                                                                    { "items are" if gdf.shape[0] > 1 else "item is"}
-                                                                    loaded on the map.
+                                                                    Your request has been processed. Nothing was found.
+                                                                    Please refine your request and try again if you think
+                                                                    this is a mistake.
                                                                     """
-                                                else:
-                                                    message = f"""
-                                                                Your request has been processed. Nothing was found.
-                                                                Please refine your request and try again if you think
-                                                                this is a mistake.
-                                                                """
                                     count_end = len(st.session_state.datasets)   
                                     for idx in range(count_start, count_end):
                                         st.session_state.datasets[idx].time = time.time()
