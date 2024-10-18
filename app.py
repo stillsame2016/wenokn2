e35d84702cff129c86d542f49c55ec129725a727
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-18 00:29:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -319,7 +319,7 @@ with col2:
     
     with open( "./style.css" ) as css:
         st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
-    selected_item = st.selectbox("", 
+    selected_item = st.selectbox(" ", 
                                  sample_queries,
                                  index=None,
                                  label_visibility='hidden',
