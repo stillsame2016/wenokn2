a1ded1a5675035d35f34fcb8edfd1e1b1cf67215
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-17 18:42:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,8 @@ def execute_query(user_input, chat_container):
     message = None
     if response.status_code == 200:
         query_plan = json.loads(response.text)
-        # st.code(json.dumps(query_plan, indent=4))
+        st.code(json.dumps(query_plan, indent=4))
+        time.sleep(20)
         if len(query_plan) > 1:
             # show the query plan
             query_plan_text = "We use the following query plan for your request:\n"
