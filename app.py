362e2aa50d12b64c251ed08556fdcbe79d2e1ae3
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-19 08:17:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ with col2:
                                  sample_queries,
                                  index=None,
                                  label_visibility='hidden',
-                                 placeholder="Select a sample query to edit and customize as needed",
+                                 placeholder="Select a sample query to edit and run as needed",
                                  key='selection_index')
     if selected_item:
         st.session_state.sample_query = selected_item  
