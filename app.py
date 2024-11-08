a2b497321dec726710f5766da912a4f2908d2fa0
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-08 15:09:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -475,8 +475,8 @@ with col2:
                             else:
                                 code = process_energy_atlas_request(llm, user_input, st.session_state.datasets)
                                 code = strip_code(code)
-                                # st.code(code)
-                                # time.sleep(20)
+                                st.code(code)
+                                time.sleep(20)
                                 exec(code)
                                 if gdf.shape[0] > 0:
                                     if hasattr(gdf, 'answer'):
