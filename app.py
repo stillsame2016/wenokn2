29408c1c2d237df21a017b48a3c05b97e5cd9a13
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-08 22:04:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -378,8 +378,8 @@ with col2:
             st.chat_message("user").markdown(user_input)
             st.session_state.chat.append({"role": "user", "content": user_input})
             route = get_question_route(llm, user_input)
-            st.markdown(route)
-            time.sleep(15)
+            # st.markdown(route)
+            # time.sleep(15)
             if route['request_type'] == 'WEN-KEN database':
                 refined_request = get_refined_question(llm, user_input)
                 # st.code(refined_request)
@@ -475,15 +475,15 @@ with col2:
                             else:
                                 code = process_energy_atlas_request(llm, user_input, st.session_state.datasets)
                                 code = strip_code(code)
-                                exec(code)
-                                # st.code(code)
-                                # time.sleep(20)
-                                # try:
-                                #     exec(code)
-                                # except Exception as e:
-                                #     error_stack = traceback.format_exc()
-                                #     st.code(error_stack)
-                                #     time.sleep(20)
+                                # exec(code)
+                                st.code(code)
+                                time.sleep(10)
+                                try:
+                                    exec(code)
+                                except Exception as e:
+                                    error_stack = traceback.format_exc()
+                                    st.code(error_stack)
+                                    time.sleep(20)
                                 if gdf.shape[0] > 0:
                                     if hasattr(gdf, 'answer'):
                                         message = gdf.answer
