032b093a194908ee72f231fbd5d01a694df8669e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-11 08:07:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -382,7 +382,7 @@ with col2:
         'Find all watersheds feed into Muskingum River',
         'Find all watersheds in Ross County in Ohio State',
         'Find the watershed with the name Headwaters Black Fork Mohican River',
-        'Find all stream gages in the watershed with the name Headwaters Black Fork Mohican River',
+        'Find all stream gages in the watershed with the name Meigs Creek',
 
         ######## NPDES ########
         'How do I determine if my facility is subject to NPDES regulations in Ohio?',
