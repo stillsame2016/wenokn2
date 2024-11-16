ea3cf17d986bfc509a1dca776d4d89a7a6af4b14
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-16 11:57:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -425,8 +425,8 @@ with col2:
             st.chat_message("user").markdown(user_input)
             st.session_state.chat.append({"role": "user", "content": user_input})
             route = get_question_route(llm, user_input)
-            # st.markdown(route)
-            # time.sleep(15)
+            st.markdown(route)
+            time.sleep(15)
             if route['request_type'] == 'WEN-KEN database':
                 refined_request = get_refined_question(llm, user_input)
                 # st.code(refined_request)
