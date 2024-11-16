c2efa13e8de77cd5bc03520e1a2d2ea389d3770f
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-16 12:11:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -425,8 +425,8 @@ with col2:
             st.chat_message("user").markdown(user_input)
             st.session_state.chat.append({"role": "user", "content": user_input})
             route = get_question_route(llm, user_input)
-            st.markdown(route)
-            time.sleep(15)
+            # st.markdown(route)
+            # time.sleep(15)
             if route['request_type'] == 'WEN-KEN database':
                 refined_request = get_refined_question(llm, user_input)
                 # st.code(refined_request)
@@ -524,9 +524,11 @@ with col2:
                             else:
                                 code = process_energy_atlas_request(llm, user_input, st.session_state.datasets)
                                 code = strip_code(code)
+                                st.code(code)
+                                time.sleep(10)
+                                
                                 exec(code)
-                                # st.code(code)
-                                # time.sleep(10)
+
                                 # try:
                                 #     exec(code)
                                 # except Exception as e:
