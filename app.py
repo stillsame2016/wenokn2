06bff2e7c307bc230cefb42bb1353a72cb636922
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-15 22:14:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -380,7 +380,7 @@ with col2:
         'Load all wind power plants with total megawatt capacity greater than 100 in California.' ,
 
         ######## Basin ########
-        'Find the basin French Broad-Holston',
+        'Find the basin Lower Ohio-Salt',
         'Find all rivers that flow through the Roanoke basin.',
         
         ######## Watershed ########
