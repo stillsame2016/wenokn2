484fcdeb21f35d96ba64164e20d8c62c84e48c3a
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-11-16 17:20:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -439,6 +439,7 @@ with col2:
                     for request in plan['requests']:
                         exist_json = spatial_dataset_exists(llm, request, st.session_state.datasets)
                         st.code(exist_json)
+                        time.sleep(10)
                         if not exist_json['existing']:
                             process_data_request(request, chat_container)
                         else:
