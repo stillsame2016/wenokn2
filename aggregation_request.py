bab20158982c074d72d23f800f746df9345832ea
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-02-03 14:18:08 -0800

    Update aggregation_request.py

--- aggregation_request.py
+++ aggregation_request.py
@@ -128,11 +128,10 @@ def get_code_for_grouping_object(llm, request):
     if request["data_source"] == "WEN-OKN database":
         response = requests.get(f"https://sparcal.sdsc.edu/api/v1/Utility/wenokn_llama3?query_text={request['request']}")
         sparql_query = response.text
-
         code = """
             endpoint = f"http://132.249.238.155/repositories/wenokn_ohio_all"
             df = sparql_dataframe.get(endpoint, '''{sparql_query}''')
             gdf = df_to_gdf(df, {request['request']})
         """
-    
+        return code
     return "OKAY"
