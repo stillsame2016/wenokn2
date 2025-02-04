bd347a6716b8e0dd2826a384e501fc8a3ef33fcc
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-02-03 20:10:26 -0800

    Update aggregation_request.py

--- aggregation_request.py
+++ aggregation_request.py
@@ -184,5 +184,5 @@ User request: {user_input}
                                      "grouping_gdf_request": grouping_gdf.label,
                                      "summarizing_gdf_columns": str(summarizing_object_gdf.columns.to_list()),
                                      "summarizing_request": summarizing_object_gdf.label,
-                                     "user_input": summarizing_object_request})
+                                     "user_input": user_input})
     return result
