ce601d9806d5f9670fa9a6b05ebd1e16c7ae4e39
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-02-24 22:06:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -748,9 +748,6 @@ with col2:
                             logger.info(code_for_aggregation)
                             st.markdown(f"**Executing the following code:**")
                             st.code(code_for_aggregation)
-
-                            if summarizing_object_gdf.crs != grouping_gdf.crs:
-                                summarizing_object_gdf = summarizing_object_gdf.to_crs(grouping_gdf.crs)
                             
                             globals_dict['grouping_gdf'] = grouping_gdf
                             globals_dict['summarizing_object_gdf'] = summarizing_object_gdf
