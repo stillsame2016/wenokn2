4b77d77275f041e9243aa7d5047816d23ae3c0e4
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-02-24 21:54:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -748,7 +748,6 @@ with col2:
                             logger.info(code_for_aggregation)
                             st.markdown(f"**Executing the following code:**")
                             st.code(code_for_aggregation)
-                            time.sleep(30)
                             
                             globals_dict['grouping_gdf'] = grouping_gdf
                             globals_dict['summarizing_object_gdf'] = summarizing_object_gdf
