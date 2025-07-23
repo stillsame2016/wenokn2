5ec37ab3ead606e8dc61af738d102387ee70bd69
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-07-22 20:11:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -440,8 +440,10 @@ with col2:
         'Find all census blocks within the 3 miles distance to the power station dpq07u6nxsp4',
 
         'Retrieve all power stations in Ohio that are flooded at 2 PM on July 1, 2025',
-        'Find all flooded power stations in Ohio from 2 PM to 6 PM on 2025-07-14',
+        'Find the tracts of all power stations in Ohio that are flooded at 2 PM on July 1, 2025', 
         'Find the populations of the tracts of all power stations in Ohio that are flooded at 2 PM on July 1, 2025',
+        
+        'Find all flooded power stations in Ohio from 2 PM to 6 PM on 2025-07-14',
         'Find social vulnerability index of the tracts of all power stations in Ohio that are flooded from 2 PM to 10 PM on July 12, 2025',
     ]
     st.markdown(
