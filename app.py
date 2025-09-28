42e8126d02fb29496ac651c24d38b203d2d7f86e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-09-28 15:19:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -469,7 +469,7 @@ with col2:
         'Find all FRS sewage treatment facilities in Ohio',
         'Find all FRS water supply and irrigation facilities in Maine',
         'Find all FRS waste treatment and disposal facilities in Illinois',
-        'Find all PFSA contamination observations within 100 meters from FRS water supply and irrigation facilities in Maine',
+        'Find all PFSA contamination observations within 800 meters from FRS water supply and irrigation facilities in Maine',
     ]
     st.markdown(
         """
