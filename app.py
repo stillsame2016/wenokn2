7e7341bbeac17f6906e6bd9950384848e66eff63
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-10-08 11:58:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ llm2 = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=
 st.set_page_config(layout="wide", page_title="WEN-OKN")
 
 # Set up the title
+st.markdown("### &nbsp; WEN-OKN")
 # st.markdown("### &nbsp; WEN-OKN: Dive into Data, Never Easier")
 # st.markdown("### &nbsp; Dive into Data, Never Easier")
 
