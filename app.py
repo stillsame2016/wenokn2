bfdfa4441833ecfe861cc2ee80d5dcfce932ff11
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-11-11 22:17:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ llm2 = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile", api_key=Gro
 llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
 llm2 = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
 
-# llm = ChatOpenAI(
-#     model="gpt-4.1-mini", temperature=0, max_tokens=5000,
-#     api_key = st.secrets["NRP_KEY"],
-#     base_url = "https://ellm.nrp-nautilus.io/v1"
-# )
+llm = ChatOpenAI(
+    model="qwen3", temperature=0, max_tokens=5000,
+    api_key = st.secrets["NRP_KEY"],
+    base_url = "https://ellm.nrp-nautilus.io/v1"
+)
 
 # Set the wide layout of the web page
 st.set_page_config(layout="wide", page_title="WEN-OKN")
