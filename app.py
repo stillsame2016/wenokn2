868eb1f7e02704a29864447605d2bc9d880f558e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-11-11 22:21:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -41,18 +41,12 @@ OpenAI_KEY = st.secrets["OpenAI_KEY"]
 # llm = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY)
 # llm2 = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY_2)
 
-llm = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile", api_key=Groq_KEY)
-llm2 = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile", api_key=Groq_KEY_2)
+llm = ChatGroq(temperature=0, model_name="qwen/qwen3-32b", api_key=Groq_KEY)
+llm2 = ChatGroq(temperature=0, model_name="qwen/qwen3-32b", api_key=Groq_KEY_2)
 
 # gpt-4o
-llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
-llm2 = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
-
-llm = ChatOpenAI(
-    model="qwen3", temperature=0, max_tokens=5000,
-    api_key = st.secrets["NRP_KEY"],
-    base_url = "https://ellm.nrp-nautilus.io/v1"
-)
+# llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
+# llm2 = ChatOpenAI(model="gpt-4.1-mini", temperature=0, max_tokens=5000, api_key=OpenAI_KEY)
 
 # Set the wide layout of the web page
 st.set_page_config(layout="wide", page_title="WEN-OKN")
