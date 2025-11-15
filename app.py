347d1274e9d669713849a527470cc7310e52140f
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-11-14 20:11:12 -0800

    Add load_rivers_in_counties function to app.py

--- app.py
+++ app.py
@@ -639,6 +639,7 @@ with col2:
                                             'load_neighboring_counties_from_names': load_neighboring_counties_from_names,
                                             'load_neighboring_states': load_neighboring_states,
                                             'load_rivers_in_county': load_rivers_in_county,
+                                            'load_rivers_in_counties': load_rivers_in_counties,
                                             'load_rivers_in_state': load_rivers_in_state,
                                             'load_counties_river_flows_through': load_counties_river_flows_through,
                                             'load_states_river_flows_through': load_states_river_flows_through,
