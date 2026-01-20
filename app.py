bb08cd8361ae1bd7d4cc48eb8272b3bb2ad073de
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2026-01-20 08:51:16 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 NEW_APP_URL = "https://deep-wenokn.streamlit.app"
 
-st.markdown(
+components.html(
     f"""
     <script>
-        window.location.replace("{NEW_APP_URL}");
+        window.location.href = "{NEW_APP_URL}";
     </script>
     """,
-    unsafe_allow_html=True,
+    height=0,
 )
+st.stop()
