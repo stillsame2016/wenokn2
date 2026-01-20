af86cb88f462c92e846f1cddf7be334b72c2a7c5
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2026-01-20 08:54:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 import streamlit as st
 
-st.markdown(
-    """
-    <script>
-    window.location.replace("https://deep-wenokn.streamlit.app/");
-    </script>
-    <noscript>
-        <meta http-equiv="refresh" content="0; url=https://deep-wenokn.streamlit.app/">
-        <p>Redirecting to the new app... If nothing happens, <a href="https://deep-wenokn.streamlit.app/">click here</a>.</p>
-    </noscript>
-    """,
-    unsafe_allow_html=True
-)
+st.markdown('<meta http-equiv="refresh" content="0;url=https://deep-wenokn.streamlit.app/">', unsafe_allow_html=True)
+st.markdown('<script>window.location.replace("https://deep-wenokn.streamlit.app/");</script>', unsafe_allow_html=True)
